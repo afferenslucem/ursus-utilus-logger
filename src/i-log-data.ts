80e1203ff9d49bd2798fffd71dba90ca56f15f5e
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-05 16:38:21 +0500

    added comments

--- src/i-log-data.ts
+++ src/i-log-data.ts
@@ -1,7 +1,22 @@
 export interface ILogData {
+    /**
+     * Log id. String what can identify message, logger, or client
+     */
     id?: string;
+    /**
+     * Log message
+     */
     message: string;
+    /**
+     * Additional log data
+     */
     obj?: any;
+    /**
+     * Logger namespace
+     */
     namespace?: string;
+    /**
+     * Loger name
+     */
     loggerName: string;
 }
